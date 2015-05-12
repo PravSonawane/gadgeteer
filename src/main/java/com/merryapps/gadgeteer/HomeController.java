2cb41e56e0d6071dddc9eb4c181a61cee6554194
PravinSonawane <june.pravin@gmail.com>
2015-05-11 23:19:49 -0400

    Created a working REST API example. /gadgeteer now returns a JSON response

--- src/main/java/com/merryapps/gadgeteer/HomeController.java
+++ src/main/java/com/merryapps/gadgeteer/HomeController.java
@@ -4,10 +4,16 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.merryapps.gadgeteer.core.Person;
 
 /**
  * Handles requests for the application home page.
@@ -20,11 +26,12 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		return "home";
+	@RequestMapping(value = "/", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Person> home() {
+		Person p = new Person(1, "Person1");
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		return new ResponseEntity<Person>(p, headers, HttpStatus.OK);
 	}
 	
 }
